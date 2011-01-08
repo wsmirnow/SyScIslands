a2e7356bc5341977e4a1ba9ef6a278f657ca3548
ketos <timwess@googlemail.com>
2011-01-08 17:03:20 +0100

    frs

--- src/SyScIslands/Insel.java
+++ src/SyScIslands/Insel.java
@@ -3,22 +3,23 @@ package SyScIslands;
 import eawag.model.Swarm;
 
 public class Insel extends Swarm {
-	
+
 	public static int autoid = 0;
-	
+	public float maxHolz = 10f;
+
 	int id;
 
-	
 	public Insel() {
 		this.id = autoid++;
-		System.out.println("Insel mit der id "+id+" wurde erstellt");
+		System.out.println("Insel mit der id " + id + " wurde erstellt");
 	}
-	
+
 	public int getGroesse() {
 		return getChildCount();
 	}
-	
+
+	@Override
 	public void action() {
-		//System.out.println("meine id ist "+id);
+		// System.out.println("meine id ist "+id);
 	}
 }
--- src/SyScIslands/Karte.java
+++ src/SyScIslands/Karte.java
@@ -1,19 +1,24 @@
 package SyScIslands;
+
 import eawag.grid.Bug;
 import eawag.grid.Grid;
 
 public class Karte extends Grid {
-	
-	
-	public float maxGamePF = 10f;
-	public float maxWoodPF = 10f;
+
+	public float maxWild = 10f;
+	public float minWild = 0f;
+
+	public float maxHolz = 10f;
+	public float minHolz = 0f;
+
 	public float freshWaterProp = 0.2f;
-	
+
 	public float gameRegeneration = 0.1f;
 	public float woodRegeneration = 0.1f;
-	
+
+	@Override
 	public void action() {
-		
+
 		if (getTop().getTime() == 0) {
 			// suche benachbarten Landfelder und erstelle daraus eine Insel
 			for (int x = 0; x < getXSize(); x++) {
@@ -32,23 +37,24 @@ public class Karte extends Grid {
 			}
 		}
 	}
-	
+
 	/**
-	 * Sucht rekursiv nach direkt benachbaten LandFeldern, 
-	 * die noch zu keiner Insel gehoeren und fuegt sie der Insel hinzu.
-	 * Dieser Vorgang wird so lange durchgefuehrt, 
-	 * bis keine Insel-freien Nachbarfelder mehr da sind.
+	 * Sucht rekursiv nach direkt benachbaten LandFeldern, die noch zu keiner
+	 * Insel gehoeren und fuegt sie der Insel hinzu. Dieser Vorgang wird so
+	 * lange durchgefuehrt, bis keine Insel-freien Nachbarfelder mehr da sind.
 	 * 
-	 * @param insel Insel, welche um weitere Landfelder wachsen soll
-	 * @param feld bei welchem die Nachbarn gesucht werden sollen
+	 * @param insel
+	 *            Insel, welche um weitere Landfelder wachsen soll
+	 * @param feld
+	 *            bei welchem die Nachbarn gesucht werden sollen
 	 */
 	void sucheLandNachbarn(Insel insel, LandFeld feld) {
 		int[] dx = MOORE_DX;
 		int[] dy = MOORE_DX;
-		
-		for (int x = 0; x < dx.length; x++) 
+
+		for (int x = 0; x < dx.length; x++)
 			for (int y = 0; y < dy.length; y++) {
-				Bug b = getBug(feld.x+dx[x], feld.y+dy[y], 1);
+				Bug b = getBug(feld.x + dx[x], feld.y + dy[y], 1);
 				if (b != null && b instanceof LandFeld) {
 					LandFeld nachbar = (LandFeld) b;
 					if (nachbar.insel == null) {
